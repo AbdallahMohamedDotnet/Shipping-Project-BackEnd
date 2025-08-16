fd634df005ae2535cb4db12c41eba0ec481b9e85
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-08-16 13:58:16 +0300

    handling some isssue on new structure on shipping Logic

--- BL/Services/Shipment/ShipmentService.cs
+++ BL/Services/Shipment/ShipmentService.cs
@@ -4,9 +4,9 @@ using BL.Contract.Shipment;
 using BL.Contracts;
 using BL.DTOConfiguration;
 using DAL.Contracts;
-using DAL.Repositories;
+
 using Domains;
-//using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,56 +15,28 @@ using System.Threading.Tasks;
 
 namespace BL.Services
 {
-    public class ShipmentService : BaseService<TbShipment, DTOShipment>, IShipment
+    public class ShipmentService : BaseServices<TbShipment, DTOShipment>, IShipment
     {
         IUserReceiver _userReceiver;
         IUserSender _userSender;
         ITrackingNumberCreator _trackingCreator;
         IRateCalculator _rateCalculator;
-       // IUnitOfWork _uow;
-        public ShipmentService(ITableRepository<TbShipment> repo,IMapper mapper,
+        ITableRepository<TbShipment> _repo;
+        public ShipmentService(ITableRepository<TbShipment> repo, IMapper mapper,
              IUserService userService, IUserReceiver userReceiver,
              IUserSender userSender, ITrackingNumberCreator trackingCreator
-            , IRateCalculator rateCalculator,/*IUnitOfWork uow*/) : base(/*uow,*/ mapper, userService)
+            , IRateCalculator rateCalculator/*, IUnitOfWork uow*/) : base(repo, mapper, userService)
         {
-            //_uow = uow;
-            _userReceiver = userReceiver;
-            _userSender = userSender;
-            _trackingCreator = trackingCreator;
-            _rateCalculator = rateCalculator;
+            this._repo = repo;
+            this._userReceiver = userReceiver;
+            this._userSender = userSender;
+            this._trackingCreator = trackingCreator;
+            this._rateCalculator = rateCalculator;
         }
-
-        public async Task Create(DTOShipment DTO )
+        public async Task Create(DTOShipment DTO)
         {
-            try
-            {
-                await _uow.BeginTransactionAsync();
-                // create tracking number
-                DTO.TrackingNumber = _trackingCreator.Create(DTO);
-                // calculate date
-                DTO.ShippingRate = _rateCalculator.Calculate(DTO);
-                // save sender
-                if (DTO.SenderId == Guid.Empty)
-                {
-                    Guid gSenderId = Guid.Empty;
-                    _userSender.Add(DTO.UserSender, out gSenderId);
-                    DTO.SenderId = gSenderId;
-                }
-                // save receiver
-                if (DTO.ReceiverId == Guid.Empty)
-                {
-                    Guid gReciverId = Guid.Empty;
-                    _userReceiver.Add(DTO.UserReceiver, out gReciverId);
-                    DTO.ReceiverId = gReciverId;
-                }
-                // save shipment
-                this.Add(DTO);
-                await _uow.CommitAsync();
-            }
-            catch(Exception ex)
-            {
-                await _uow.RollbackAsync();
-            }
+
+
         }
     }
 }
